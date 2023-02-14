ef05da8078c54234fc11f7ed3d8744c0411423f2
betulnecanli <betulnecanli@gmail.com>
2023-02-14 12:55:08 +0300

    Constants file added.

---
+++ app/src/main/java/com/betulnecanli/sailormoonapp/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.betulnecanli.sailormoonapp.utils
+
+object Constants {
+
+    const val DETAILS_ARGUMENT_KEY = "chId"
+    const val SAILOR_DB = "sailor_database"
+    const val SAILOR_DATABASE_TABLE = "sailor_table"
+    const val SAILOR_REMOTE_KEY_DATABASE_TABLE = "sailor_remote_key_table"
+}
