71404d6943c092a9a82d0846473b2e025b977ebd
betulnecanli <betulnecanli@gmail.com>
2023-02-14 16:50:00 +0300

    Implementations updated.

--- app/src/main/java/com/betulnecanli/sailormoonapp/di/AppModule.kt
+++ app/src/main/java/com/betulnecanli/sailormoonapp/di/AppModule.kt
@@ -1,8 +1,8 @@
-package com.betulnecanli.di
+package com.betulnecanli.sailormoonapp.di
 
 import android.content.Context
 import androidx.room.Room
-import com.betulnecanli.data.local.SailorDatabase
+import com.betulnecanli.sailormoonapp.data.local.SailorDatabase
 import com.betulnecanli.sailormoonapp.utils.Constants.SAILOR_DB
 import dagger.Module
 import dagger.Provides
