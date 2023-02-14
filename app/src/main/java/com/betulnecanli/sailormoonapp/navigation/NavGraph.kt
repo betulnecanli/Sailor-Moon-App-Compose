9b3dc6b39a3f78c1468dbca05e96177b6f4eda16
betulnecanli <betulnecanli@gmail.com>
2023-02-14 13:05:46 +0300

    Navigation files added.

---
+++ app/src/main/java/com/betulnecanli/sailormoonapp/navigation/NavGraph.kt
@@ -0,0 +1,37 @@
+package com.betulnecanli.sailormoonapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.betulnecanli.presentation.screens.splash.SplashScreen
+import com.betulnecanli.sailormoonapp.utils.Constants.DETAILS_ARGUMENT_KEY
+
+@Composable
+fun SetupNavGraph(navController: NavHostController){
+    NavHost(navController = navController, startDestination = Screen.Splash.route){
+        composable(route = Screen.Splash.route){
+            SplashScreen(navController = navController)
+        }
+        composable(route = Screen.Welcome.route){
+
+        }
+        composable(route = Screen.Home.route,
+            arguments = listOf(navArgument(DETAILS_ARGUMENT_KEY){
+                type = NavType.IntType
+            })
+        ){
+
+        }
+        composable(route = Screen.Details.route){
+
+        }
+        composable(route = Screen.Search.route){
+
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/betulnecanli/sailormoonapp/navigation/Screen.kt
@@ -0,0 +1,14 @@
+package com.betulnecanli.sailormoonapp.navigation
+
+sealed class Screen(val route: String){
+    object Splash :Screen("splash_screen")
+    object Welcome : Screen("welcome_screen")
+    object Home :Screen("home_screen")
+    object Details : Screen("details_screen"){
+        fun passCharacterId(chId : Int) : String {
+            return "details_screen/$chId"
+        }
+    }
+
+    object Search : Screen("search_screen")
+}
