e701fbab13d6f3ba82032f9d9b6f84ecc2777d0d
betulnecanli <betulnecanli@gmail.com>
2023-02-14 13:05:30 +0300

    New colors added.

--- app/src/main/java/com/betulnecanli/sailormoonapp/ui/theme/Color.kt
+++ app/src/main/java/com/betulnecanli/sailormoonapp/ui/theme/Color.kt
@@ -2,6 +2,11 @@ package com.betulnecanli.sailormoonapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val Pink200 = Color(0xFFE77B9F)
+val Pink500 = Color(0xFFE91E63)
+val Pink700 = Color(0xFFB90945)
+val Teal200 = Color(0xFF3F51B5)
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
