c96a83a3572ff5bdcb3562790e0c2c9687ea9e03
betulnecanli <betulnecanli@gmail.com>
2023-02-16 22:55:16 +0300

    GetAllCharacters UseCase added.

--- app/src/main/java/com/betulnecanli/sailormoonapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/betulnecanli/sailormoonapp/presentation/screens/home/HomeScreen.kt
@@ -3,10 +3,16 @@ package com.betulnecanli.sailormoonapp.presentation.screens.home
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(){
+fun HomeScreen(
+        homeViewModel: HomeViewModel = hiltViewModel()
+){
+
+        val allCharacters = homeViewModel.getAllCharacters
+
         Scaffold(topBar = {
                 HomeTopBar(onSearchClicked = {})
         }){}
---
+++ app/src/main/java/com/betulnecanli/sailormoonapp/presentation/screens/home/HomeViewModel.kt
@@ -0,0 +1,16 @@
+package com.betulnecanli.sailormoonapp.presentation.screens.home
+
+import androidx.lifecycle.ViewModel
+import com.betulnecanli.sailormoonapp.domain.use_cases.UseCases
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+        useCases: UseCases
+): ViewModel() {
+
+    val getAllCharacters  = useCases.getAllCharactersUseCase
+
+}
