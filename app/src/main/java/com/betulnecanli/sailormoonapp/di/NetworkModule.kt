4c825a0ab7f3589aceb7aa7e1f95a49369989b48
“betul” <betulnecanli@gmail.com>
2023-07-29 12:51:26 +0300

    Chucker library implemented.

--- app/src/main/java/com/betulnecanli/sailormoonapp/di/NetworkModule.kt
+++ app/src/main/java/com/betulnecanli/sailormoonapp/di/NetworkModule.kt
@@ -1,18 +1,22 @@
 package com.betulnecanli.sailormoonapp.di
 
+import android.content.Context
 import com.betulnecanli.sailormoonapp.data.local.SailorDatabase
 import com.betulnecanli.sailormoonapp.data.remote.SailorApi
 import com.betulnecanli.sailormoonapp.data.repository.RemoteDataSourceImpl
 import com.betulnecanli.sailormoonapp.domain.repository.RemoteDataSource
 import com.betulnecanli.sailormoonapp.utils.Constants.BASE_URL
+import com.chuckerteam.chucker.api.ChuckerInterceptor
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import java.util.concurrent.TimeUnit
@@ -23,10 +27,17 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
+
+    @Provides
+    fun provideApplicationContext(@ApplicationContext appContext: Context): Context {
+        return appContext
+    }
+
     @Provides
     @Singleton
-    fun provideHttpClient(): OkHttpClient{
+    fun provideHttpClient(context : Context): OkHttpClient{
         return OkHttpClient.Builder()
+            .addInterceptor(ChuckerInterceptor(context))
             .readTimeout(15, TimeUnit.SECONDS)
             .connectTimeout(15, TimeUnit.SECONDS)
             .build()
@@ -37,7 +48,7 @@ object NetworkModule {
     @Provides
     @Singleton
     fun provideRetrofitInstance(okHttpClient: OkHttpClient): Retrofit{
-       val contentType = MediaType.get("application/json")
+       val contentType = "application/json".toMediaType()
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(okHttpClient)
